a1f05be7e62a22397e127821c7ea9553f9301ed0
Husni Kamal <husnikamal3@gmail.com>
2024-07-18 13:55:19 +0700

    add utils times

--- __tests__/utils/math.test.ts
+++ __tests__/utils/math.test.ts
@@ -1,7 +1,15 @@
-import { add } from "@/utils/math";
+import { add, times } from "@/utils/math";
 
 describe("math", () => {
   it("add", () => {
     expect(add(2, 3)).toBe(5);
+  });
+
+  test("times", () => {
+    expect(times(2, 3)).toBe(6);
+  });
+
+  test("times negative case", () => {
+    expect(times(2, 3)).not.toBe(7);
   });
 });
--- utils/math.ts
+++ utils/math.ts
@@ -1,3 +1,7 @@
 export function add(x: number, y: number) {
   return x + y;
 }
+
+export function times(x: number, y: number) {
+  return x * y;
+}
